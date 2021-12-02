2b8a47e72959faa6aa1cab82a440f8f23d7ef8a2
avanlinden <11965371+avanlinden@users.noreply.github.com>
2021-12-02 23:31:24 +0000

    add expected snrnaseq specimens

--- R/join-all-rosmap-specimens.R
+++ R/join-all-rosmap-specimens.R
@@ -2,50 +2,54 @@
 
 # ROSMAP clinical file
 clinical <- syn$get("syn3191087")$path %>% 
-  read_csv(col_select = c(individualID))
+  read_csv(col_select = c(individualID, projid))
 
 # ROSMAP biospecimen file
 rosmap <- syn$get("syn21323366")$path %>% 
   read_csv(col_select = c(individualID, specimenID, organ, tissue, assay)) %>% 
-  mutate(study = "ROSMAP")
+  mutate(study = "ROSMAP",
+         dataStatus = "received")
 
 # satellite specimen info
 
 satellites <- syn$get("syn26522612")$path %>% 
-  read_csv()
+  read_csv() %>% 
+  mutate(dataStatus = "received")
+
+# expected donors from upcoming snRNAseq
+
+exp_projids <- syn$get("syn26524079")$path %>% 
+  read_table() 
+
+# no leading zero problems
+exp_projids %>% 
+  filter(nchar(projid) < 8)
+
+# join to individualIDs in clinical file
+# create dummy specimen IDs
+exp_rosmap <- exp_projids %>% 
+  left_join(clinical, by = "projid") %>% 
+  mutate(specimenID = paste0(individualID, "_expected_snRNAseq"),
+         assay = "snrnaSeq",
+         organ = "brain",
+         tissue = "dorsolateral prefrontal cortex",
+         dataStatus = "expected",
+         study = "expected_snrnaSeq") %>% 
+  select(colnames(satellites))
 
 # join all rosmap specimen info
-all_rosmap <- rosmap %>% 
+# filter out individuals not in the clinical file
+rosmap_combined <- rosmap %>% 
   select(colnames(satellites)) %>% 
-  bind_rows(satellites)
-
-# which of these aren't in the clinical file?
-# 291 from ROSMAP - at least 100 of the TMT are pools, many are just missing, mostly rnaCounts. Look for this
-missing_rosmap_individuals <- all_rosmap %>% 
-  filter(!individualID %in% clinical$individualID) %>% 
-  filter(study == "ROSMAP") %>% 
-  group_by(assay) %>% 
-  count()
-
-all_rosmap %>% 
-  filter(!individualID %in% clinical$individualID) %>% 
-  filter(study == "ROSMAP_CognitiveResilience") # these are just TMT controls
-
-all_rosmap %>% 
-  filter(!individualID %in% clinical$individualID) %>% 
-  filter(study == "ROSMAP_nucleus_hashing") # these are just mice
-
-# only ones missing are OG ROSMAP
-# remove for now
-
-rosmap_combined <- all_rosmap %>% 
+  bind_rows(satellites) %>% 
+  bind_rows(exp_rosmap) %>% 
   filter(individualID %in% clinical$individualID)
 
 # set up datatype by assay groups
 # LC-MSMS is from Emory Lipidomics, so is metabolomics
 # "label free mass spec" is proteomics
 
-geneExpression <- c("scrnaSeq", "mRNAcounts", "rnaSeq", "rnaArray", "snrnaSeq")
+geneExpression <- c("scrnaSeq", "mirnaArray", "rnaSeq", "rnaArray", "snrnaSeq")
 epigenetics <- c("methylationArray", "ChIPSeq", "bisulfiteSeq")
 genomicVariants <- c("snpArray", "wholeGenomeSeq")
 metabolomics <- c("Biocrates p180", "Biocrates Bile Acids", "Metabolon", "LC-MSMS")
